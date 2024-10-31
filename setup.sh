3c46fcfca20fd9656ca3806f1257532b5f9d4d51
rhabimana <habimanarobert5@gmail.com>
2024-10-31 16:41:53 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# sets up the virtual environment 
+python -m venv venv 
+
+
+# installs packages in the requirement file 
+pip install -r requirements.txt
+
+
+# creates source folder 
+mkdir src
+
+
+# creates data folder 
+mkdir data
